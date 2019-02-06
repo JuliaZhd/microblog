5f37ccff40297f4ae3aeceb82078eca37bcf6d7b
JuliaZhd <fastomama@gmail.com>
2019-02-06 19:02:56 +0200

    +avatar

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -33,9 +34,8 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password) 
 
     def avatar(self, size):
-        #digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return ('ava_geom.png', size)
-        #'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)      
 
     def follow(self, user):
         if not self.is_following(user):
